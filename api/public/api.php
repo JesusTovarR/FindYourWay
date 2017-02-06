190d13b8ad74b287e3325f2ffb371cc72942e902
JesusTovarR <jesustovar678@gmail.com>
2017-02-06 17:02:38 +0100

    connexion

---
+++ api/public/api.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 13/12/16
+ * Time: 16:34
+ */
+use api\AppInit;
+
+require_once '../vendor/autoload.php';
+
+
+AppInit::bootEloquent('../conf/conf.ini');
+
+$configuration = [
+    'settings'=>[
+        'displayErrorDetails'=>true,
+        'production'=>false],
+];
+$configuration['notAllowedHandler'] = function ($c) {
+    return function ($request, $response, $methods) use ($c) {
+        return $c['response']
+            ->withStatus(405)
+            ->withHeader('Allow', implode(', ', $methods))
+            ->withHeader('Content-type', 'application/json')
+            ->write(json_encode(["Message"=>'Method must be one of: ' . implode(', ', $methods)]));
+    };
+};
+
+$configuration['notFoundHandler'] = function ($c) {
+    return function ($request, $response){
+        return $response->withStatus(404)
+            ->withHeader('Content-type', 'application/json')
+            ->write(json_encode(["Message"=>'URI not found']));
+    };
+};
+
+$c = new \Slim\Container($configuration);
+$app = new Slim\App($c);
+$app->get('/place/{id}', PlaceController::class. ':getPlace')->setName('place');
+$app->run();
--- api/src/AppInit.php
+++ api/src/AppInit.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace app;
+namespace api;
 
 use \Illuminate\Database\Capsule\Manager;
 
